2a9792f0c79357eda9b8b5084d05cfbe294fd477
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-05 20:19:13 -0400

    Finished updating the CRUD functions to use the API

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,6 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "GET_BLOG_POSTS":
       return action.payload;
-    case "ADD_BLOG_POST":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "EDIT_BLOG_POST":
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -36,11 +27,8 @@ const getBlogPosts = (dispatch) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({
-      type: "ADD_BLOG_POST",
-      payload: { title, content },
-    });
+  return async (title, content, callback) => {
+    await jsonServer.post("/blogposts", { title, content });
     if (callback) {
       callback();
     }
@@ -48,7 +36,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({
       type: "EDIT_BLOG_POST",
       payload: { id, title, content },
@@ -60,7 +49,8 @@ const editBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({
       type: "DELETE_BLOG_POST",
       payload: id,
